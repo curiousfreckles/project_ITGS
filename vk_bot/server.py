8f89fa1808d6bd34487403ec052bb3c675221945
TolMalware <tolmachdanil@gmail.com>
2018-11-05 22:10:07 +0300

    Add web-server for longpolls

---
+++ vk_bot/server.py
@@ -0,0 +1,23 @@
+import vk_requests
+from flask import Flask, request, json
+from settings import *
+app = Flask(__name__)
+
+@app.route('/', methods=['POST'])
+def processing():
+    #Распаковываем json из пришедшего POST-запроса
+    data = json.loads(request.data)
+    #Вконтакте в своих запросах всегда отправляет поле типа
+    if 'type' not in data.keys():
+        return 'not vk'
+    if data['type'] == 'confirmation':
+        return confirmation_token
+    elif data['type'] == 'message_new':
+        api = vk_requests.create_api()
+        user_id = data['object']['user_id']
+        api.messages.send(access_token=token, user_id=str(user_id), message='Привет, я новый бот!')
+        # Сообщение о том, что обработка прошла успешно
+        return 'ok'
+#app.run() pythonanywhere автоматом дописывает эту строку, если вы разворачиваете на своем сервере
+# то расскомитьте эту строку
+
