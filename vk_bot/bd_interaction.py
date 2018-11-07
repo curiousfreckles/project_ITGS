f1e9b19c339e8485751d1b9bd3db733a2462f4bb
TolMalware <tolmachdanil@gmail.com>
2018-11-08 01:31:50 +0300

    add dbinteraction.py

---
+++ vk_bot/bd_interaction.py
@@ -0,0 +1,42 @@
+import os
+import datetime
+import json
+
+def generate_person_bd(id):
+    now = datetime.datetime.now()
+    root_directory = os.getcwd()
+    os.mkdir(id)
+    os.chdir(id)
+    os.mkdir(now.month)
+    os.chdir(now.month)
+    weeks_db = open('weeks.json','a')
+    #weeks_write() #штука которая запишет каркас файла
+    weeks_db.close()
+    days_db = open('days.json','a')
+    #days_write() #штука которая запишет каркас файла
+    days_db.close()
+    mounth_db = open('mounth.json','a')
+    #mounth_write() #штука которая запишет каркас файла
+    mounth_db.close()
+    os.chdir(id)
+    os.chdir(root_directory)
+
+def write_task(id,name,type_task):
+    now = datetime.datetime.now()
+    task = {'name':name,'result':0}
+    
+    if type_task == "day":
+        os.chdir(id)
+        os.chdir(now.month)
+        days_db = open('days.json','r').read()
+        json_pars = json.loads(days_db)
+        json_pars[str(now.day)].append(task)
+        days_db = open('days.json','w').write(json_pars)
+
+    elif type_task == "week":
+        os.chdir(id)
+        os.chdir(now.month)
+        weeks_db = open('weeks.json','r').read()
+        json_pars = json.loads(weeks_db)
+        json_pars[str(now.isoweekday)].append(task)
+        days_db = open('weeks.json','w').write(json_pars)
