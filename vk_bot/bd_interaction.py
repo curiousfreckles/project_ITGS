180703801749b54b1d148f70cfef05603233ee11
TolMalware <tolmachdanil@gmail.com>
2018-11-08 02:04:33 +0300

    fix bugs in bd_interaction

--- vk_bot/bd_interaction.py
+++ vk_bot/bd_interaction.py
@@ -3,12 +3,13 @@ import datetime
 import json
 
 def generate_person_bd(id):
+    id = str(id)
     now = datetime.datetime.now()
     root_directory = os.getcwd()
     os.mkdir(id)
     os.chdir(id)
-    os.mkdir(now.month)
-    os.chdir(now.month)
+    os.mkdir(str(now.month))
+    os.chdir(str(now.month))
     weeks_db = open('weeks.json','a')
     #weeks_write() #штука которая запишет каркас файла
     weeks_db.close()
@@ -18,32 +19,39 @@ def generate_person_bd(id):
     mounth_db = open('mounth.json','a')
     #mounth_write() #штука которая запишет каркас файла
     mounth_db.close()
-    os.chdir(id)
     os.chdir(root_directory)
 
 def write_task(id,name,type_task):
+    id = str(id)
     now = datetime.datetime.now()
-    task = {'name':name,'result':0}
+    root_directory = os.getcwd()
+    task = {"name":name,"result":0}
 
     if type_task == "day":
         os.chdir(id)
-        os.chdir(now.month)
+        os.chdir(str(now.month))
         days_db = open('days.json','r').read()
         json_pars = json.loads(days_db)
+        print(json_pars)
+        print(now.day)
         json_pars[str(now.day)].append(task)
-        days_db = open('days.json','w').write(json_pars)
+        days_db = open('days.json','w').write(json.dumps(json_pars))
+        os.chdir(root_directory)
 
     elif type_task == "week":
         os.chdir(id)
-        os.chdir(now.month)
+        os.chdir(str(now.month))
         weeks_db = open('weeks.json','r').read()
         json_pars = json.loads(weeks_db)
         json_pars[str(now.isoweekday)].append(task)
-        days_db = open('weeks.json','w').write(json_pars)
+        days_db = open('weeks.json','w').write(json.dumps(json_pars))
+        os.chdir(root_directory)
+
     else:
         os.chdir(id)
-        os.chdir(now.month)
+        os.chdir(str(now.month))
         mounth_db = open('mounth.json','r').read()
         json_pars = json.loads(mounth_db)
         json_pars[str(now.isoweekday)].append(task)
-        mounth_db = open('mounth.json','w').write(json_pars)
+        mounth_db = open('mounth.json','w').write(json.dumps(json_pars))
+        os.chdir(root_directory)
