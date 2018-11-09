9259200c03e4a62123a9b9fa8ff4ced257abba09
TolMalware <tolmachdanil@gmail.com>
2018-11-09 21:11:53 +0300

    add write_templates()

--- vk_bot/bd_interaction.py
+++ vk_bot/bd_interaction.py
@@ -3,23 +3,43 @@ import datetime
 import json
 import year_info
 
+def change_result(id,mounth,type_task,name):
+    root_directory = os.getcwd()
+    os.chdir(str(id))
+    os.chdir(str(mounth))
+
+
+
+def write_templates():
+    for num,mounth in enumerate(year_info.info):
+        num=str(num+1)
+        os.mkdir(num)
+        os.chdir(num)
+
+        weeks_db = open('weeks.json', 'w')
+        json_db = []
+        for i in range(mounth[num]['weeks']):
+            json_db.append({str(i+1):[]})
+        weeks_db.write(json.dumps(json_db))
+        weeks_db.close()
+
+        days_db = open('days.json', 'w')
+        json_db = []
+        for i in range(mounth[num]['days']):
+            json_db.append({str(i+1):[]})
+        days_db.write(json.dumps(json_db))
+        days_db.close()
+
+        mounth_db = open('mounth.json', 'w').write(json.dumps([]))
+        os.chdir("..")
+
 def generate_person_bd(id):
     id = str(id)
     now = datetime.datetime.now()
     root_directory = os.getcwd()  # запомним корень всей БД чтобы вернуться в нее
     os.mkdir(id)
     os.chdir(id)
-    os.mkdir(str(now.month))
-    os.chdir(str(now.month))
-    weeks_db = open('weeks.json', 'a')
-    # weeks_write() #штука которая запишет каркас файла
-    weeks_db.close()
-    days_db = open('days.json', 'a')
-    # days_write() #штука которая запишет каркас файла
-    days_db.close()
-    mounth_db = open('mounth.json', 'a')
-    # mounth_write() #штука которая запишет каркас файла
-    mounth_db.close()
+    write_templates()
     os.chdir(root_directory)
 
 
@@ -35,21 +55,21 @@ def write_task(id, name, type_task):
     if type_task == "day":
         days_db = open('days.json', 'r').read()
         json_pars = json.loads(days_db)
-        json_pars[str(now.day)].append(task)
+        json_pars[now.day-1][str(now.day)].append(task)
         days_db = open('days.json', 'w').write(json.dumps(json_pars))
         os.chdir(root_directory)
 
     elif type_task == "week":
         weeks_db = open('weeks.json', 'r').read()
         json_pars = json.loads(weeks_db)
-        json_pars[str(now.day // 7 + now.day % 7)].append(task)
+        json_pars[now.day // 7-1][str(now.day // 7)].append(task)
         days_db = open('weeks.json', 'w').write(json.dumps(json_pars))
         os.chdir(root_directory)
 
     else:
         mounth_db = open('mounth.json', 'r').read()
         json_pars = json.loads(mounth_db)
-        json_pars[0].append(task)
+        json_pars.append(task)
         mounth_db = open('mounth.json', 'w').write(json.dumps(json_pars))
         os.chdir(root_directory)
 
@@ -64,13 +84,13 @@ def get_tasks(id, type_task):
 
     if type_task == "day":
         days_db = open('days.json', 'r').read()
-        json_pars = json.loads(days_db)[str(now.day)]
+        json_pars = json.loads(days_db)[now.day-1][str(now.day)]
         os.chdir(root_directory)
         return json_pars
 
     elif type_task == "week":
         weeks_db = open('weeks.json', 'r').read()
-        json_pars = json.loads(weeks_db)[str(now.day // 7 + now.day % 7)]
+        json_pars = json.loads(weeks_db)[now.day // 7-1][str(now.day //7)]
         os.chdir(root_directory)
         return json_pars
 
